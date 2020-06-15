35265bbaf7d74df67bccf472c2bfbd55af0d5268
GabrielFormarioInsper <gabrielf3@al.insper.edu.br>
2020-06-14 21:32:58 -0300

    Jogo Final

--- Tetris.py
+++ Tetris.py
@@ -6,3 +6,7 @@ from classes import *
 
 #Rodando loop principal
 main(tamanho_bloco)
+
+# Gostaria de ressaltar aqui a importância que alguns, tutorias pela internet que serviram de inspiração para a realização deste projeto, 
+# entre eles vou citar alguns: freeCodeCamp.org, Tech with Tim, forum do site Pygame. Estes me ajudaram a realizar o código em diversos 
+# momentos para entender e aplicar melhor alguns códigos.
